43a4f3484c955a5cb38e14007ec437da81ff34d3
jalland <jjalland@gmail.com>
2024-01-12 13:13:47 -0600

    Finished the delete section, and added README

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path')
 const fs = require('fs');
 const notesData = require('./db/db.json');
-// Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
+const { triggerAsyncId } = require('async_hooks');
 const PORT = 3001;
 
 const app = express();
@@ -11,42 +11,42 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 
+// GET request for homepage
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
+// GET request for notes data from JSON file
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './db/db.json'));
+});
 
-app.get('/api/notes', (req, res) => res.json(notesData));
 
 // POST request to add a note
 app.post('/api/notes', (req, res) => {
-  // Log that a POST request was received
   console.info(`${req.method} request received to add a note`);
 
-  // Destructuring assignment for the items in req.body
+  // Destructuring assignment
   const { title, text } = req.body;
 
-  // If all the required properties are present
   if (title && text ) {
-    // Variable for the object we will save
     const newNote = {
       title,
       text,
-      note_id: uuid(),
+      id: uuid(),
     };
 
-    // Obtain existing notes
+    // Read in existing notes
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) {
         console.error(err);
       } else {
-        // Convert string into JSON object
         const parsedNotes = JSON.parse(data);
 
-        // Add a new note
+        // Append new note to JSON object
         parsedNotes.push(newNote);
 
-        // Write updated notes back to the file
+        // Write updated notes back to the file 
         fs.writeFile(
           './db/db.json',
           JSON.stringify(parsedNotes, null, 4),
@@ -73,50 +73,38 @@ app.post('/api/notes', (req, res) => {
 //Delete request to delete a note
 app.delete('/api/notes/:id',(req,res) => {
   console.info(`${req.method} request received to delete a note`);
-  const idToDelete = req.params.id.toLowerCase();
+  const idToDelete = req.params.id;
 
-  for (let i = 0; i < notesData.length; i++) {
-    console.log(notesData[i])
-    if (idToDelete === notesData[i].note_id.toLowerCase()) {
-      const indexToDelete = i
-      // Obtain existing notes
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) {
         console.error(err);
-      } else {
-        // Convert string into JSON object
+      } 
+      try {
         const parsedNotes = JSON.parse(data);
+        //Filter out the note to delete based on its id
+        const newArray = parsedNotes.filter(note => note.id !== idToDelete);
 
-        // Add a new note
-        const deletedNote = notesData[indexToDelete]
-        console.log("DELETED NOTE SHOULD BE:")
-        console.log(deletedNote)
-        parsedNotes.pop(deletedNote);
+      // Write updated notes back to the file
+      fs.writeFile(
+        './db/db.json',
+        JSON.stringify(newArray, null, 4),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info('Successfully deleted note!')
+      );
+      const response = {
+        status: 'success',
+        body: newArray,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
 
-        // Write updated notes back to the file
-        fs.writeFile(
-          './db/db.json',
-          JSON.stringify(parsedNotes, null, 4),
-          (writeErr) =>
-            writeErr
-              ? console.error(writeErr)
-              : console.info('Successfully deleted notes!')
-        );
+      } catch (error) {
+        console.error(error);
       }
     });
-
-    const response = {
-      status: 'success',
-      body: deletedNote,
-    };
-
-    console.log(response);
-    res.status(201).json(response);
-    }
-  }
-
-  
-  
 });
 
 app.get('*', (req, res) => {
